731590ceda2bf2d6f041ce0a1b95b8697dc9eb73
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-01-22 02:59:41 -0500

    help pls

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ def home():
 
 @app.route("/getvolume", methods=['POST'])
 def get_volume():
-    file = request.files['audio_file']
     # avgDeci = get_amplitdue(file)
     # return avgDeci
     data = request.get_data()
-    return(file)
+    return(data)
